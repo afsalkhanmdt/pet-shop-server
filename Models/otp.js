66fdd823305d933584719fd4f23887c0912ab3e9
Rojin <rojin007.rr@gmail.com>
2021-11-08 17:41:21 +0530

    file

--- Models/otp.js
+++ Models/otp.js
@@ -7,4 +7,4 @@ const otpSchema = new mongoose.Schema({
     // password: {type: String, required: true}
 })
 
-module.exports = mongoose.model('otp', otpSchema)
+module.exports = mongoose.model('Otp', otpSchema)
